2371032f7fa11fe8674e9db4edbdbc146fa110e6
candyP <2689825705@qq.com>
2026-01-19 00:33:53 +0800

    refactor: 修改redis key名称

--- src/main/java/com/example/luofushan/common/constant/RedisCacheConstant.java
+++ src/main/java/com/example/luofushan/common/constant/RedisCacheConstant.java
@@ -2,7 +2,7 @@ package com.example.luofushan.common.constant;
 
 public class RedisCacheConstant {
 
-    public static final String POST_LIKE_KEY = "luofushan:post:";
+    public static final String POST_LIKE_KEY = "luofushan:post_like:";
 
     public static final String LOCATION_CHECKIN_KEY = "luofushan:location_checkin:";
 }
